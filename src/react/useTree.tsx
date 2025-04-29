f0e0c37b54f3d601747003bf93f74a3eba973be7
nmsimons <nick@dreamlarge.com>
2025-04-29 08:53:18 -0700

    hook

--- src/react/useTree.tsx
+++ src/react/useTree.tsx
@@ -7,9 +7,7 @@ export function useTree(node: TreeNode, deep: boolean = false): object {
 	const [inval, setInval] = useState({});
 
 	useEffect(() => {
-		let eventName: "nodeChanged" | "treeChanged" = "nodeChanged";
-		if (deep) eventName = "treeChanged";
-		const unsubscribe = Tree.on(node, eventName, () => {
+		const unsubscribe = Tree.on(node, deep ? "treeChanged" : "nodeChanged", () => {
 			setInval({});
 		});
 		return unsubscribe;
